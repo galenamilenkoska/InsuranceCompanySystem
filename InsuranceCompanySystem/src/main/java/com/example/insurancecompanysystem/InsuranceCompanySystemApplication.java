8547683f594fd3ada9ba4cf65014af5cebc3df62
galenamilenkoska <94135872+galenamilenkoska@users.noreply.github.com>
2023-06-16 21:33:06 +0200

    Initial project

---
+++ InsuranceCompanySystem/src/main/java/com/example/insurancecompanysystem/InsuranceCompanySystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.insurancecompanysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceCompanySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceCompanySystemApplication.class, args);
+    }
+
+}
---
+++ InsuranceCompanySystem/src/test/java/com/example/insurancecompanysystem/InsuranceCompanySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insurancecompanysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceCompanySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
