87a91da399807fe778bb61257f16ffe8196b3fb0
Angela <angela.stevkovska3@gmail.com>
2023-06-26 01:15:16 +0200

    Added claims controller and fixed html

---
+++ InsuranceCompanySystem/src/main/java/com/example/insurancecompanysystem/web/ClaimController.java
@@ -0,0 +1,42 @@
+package com.example.insurancecompanysystem.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class ClaimController {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public ClaimController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @GetMapping("/claims")
+    public String getPolicy(@RequestParam(defaultValue = "0") int page,
+                            @RequestParam(defaultValue = "10") int pageSize,
+                            Model model) {
+        // Calculate the offset based on the page number and page size
+        int offset = page * pageSize;
+
+        // Execute a SQL query with pagination
+        String sql = "SELECT * FROM all_claims OFFSET ? LIMIT ?";
+        List<Map<String, Object>> viewData = jdbcTemplate.queryForList(sql, offset, pageSize);
+
+        // Add the view data and pagination information to the model
+        model.addAttribute("viewData", viewData);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("pageSize", pageSize);
+
+        // Call the view named "policy-view" and pass the model to it
+        return "all_claims";
+    }
+}
